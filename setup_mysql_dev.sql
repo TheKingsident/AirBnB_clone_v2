ae3a3db411c7658c1ad64738572246aedbb8547f
faty <fatyboubakri31@gmail.com>
2023-12-18 21:27:05 +0000

    task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- script that prepares a MySQL server for the project
+
+-- create database if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+
+-- create a new user with password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
+
+-- grant all privileges on the database hbnb_dev_db to hbnb_dev
+GRANT * ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+
+-- grant SELECT privilege on the database performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
