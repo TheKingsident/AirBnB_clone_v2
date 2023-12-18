dd63a0e2af39a4e452c9128343fc32bf155ab395
faty <fatyboubakri31@gmail.com>
2023-12-18 21:31:44 +0000

    task 3 fixing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,10 +5,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 
 -- create a new user with password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- grant all privileges on the database hbnb_dev_db to hbnb_dev
-GRANT * ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- grant SELECT privilege on the database performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
