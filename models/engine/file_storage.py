358728f64633f20b5a6a023a7495719e8c96132e
faty <fatyboubakri31@gmail.com>
2023-12-19 15:57:37 +0000

    TASK 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,28 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            obj_dict = {}
+            for k, v in FileStorage.__objects.items():
+                if v.to_dict()['__class__'] == cls.__name__:
+                    obj_dict.update({k: v})
+                return obj_dict
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def delete(self, obj=None):
+        """delete obj from __objects"""
+        if obj is not None:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            if key in self.all():
+                del FileStorage.__objects[key]
+                self.save()
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -45,6 +60,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
