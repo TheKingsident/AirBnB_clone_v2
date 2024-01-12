9975d84ac9d5c87f42f549fb943afa5fa1aaf2c6
TheKingsident <kingsident@gmail.com>
2024-01-12 02:36:24 +0100

    updated some files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-""" Generates a .tgz archive and deploys it to web servers. """
-from datetime import datetime
-from fabric.api import local, put, run, env
-import os
+"""
+This is the 2-do_deploy_web_static.py module.
+This module distribute the static content (html, css, images) to the servers
+"""
 
-env.hosts = ['54.236.27.95', '54.237.18.67']
-env.user = "ubuntu"
-env.key_filename = "~/RSA_public_key"
+
+from fabric.api import put, run, env, task
+import os
+env.hosts = ['52.91.182.154', '34.202.164.102']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-    if not os.path.exists(archive_path):
+    """ This is the function for deploying the content """
+    if os.path.exists(archive_path) is False:
         return False
     try:
-        put(archive_path, '/tmp/')
-        file_name = os.path.basename(archive_path)
-        name = file_name.split('.')[0]
-        release_path = "/data/web_static/releases/" + name
-        run("mkdir -p " + release_path)
-        run("tar -xzf /tmp/" + file_name + " -C " + release_path + "/")
-        run("rm /tmp/" + file_name)
-        run("mv " + release_path + "/web_static/* " + release_path + "/")
-        run("rm -rf " + release_path + "/web_static")
-        run("rm -rf /data/web_static/current")
-        run("ln -s " + release_path + "/ /data/web_static/current")
-        print("New version deployed!")
+        p = "/data/web_static/releases/"
+        file_name = archive_path.split('/')[1]
+        no_ext = file_name.split('.')[0]
+        sym_link = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/" + no_ext + "/")
+        run("tar -xzf /tmp/" + file_name + " -C " + p + no_ext + "/")
+        run("rm -rf /tmp/" + file_name)
+        run("mv " + p + no_ext + "/web_static/* " + p + no_ext + "/")
+        run("rm -rf /tmp/" + file_name)
+        run("rm -rf /data/web_static/releases/" + no_ext + "/web_static")
+        run("rm -rf " + sym_link)
+        run("ln -s /data/web_static/releases/" + no_ext + "/ " + sym_link)
         return True
     except Exception:
         return False
+    
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -15,7 +15,7 @@ def do_pack():
             local('mkdir -p versions')
         now = datetime.now().strftime('%Y%m%d%H%M%S')
         file_name = 'versions/web_static_' + now + '.tgz'
-        local('tar -czvf ' + file_name + ' -C web_static .')
+        local('tar -czvf ' + file_name + ' web_static .')
         return file_name
     except Exception:
         return None
@@ -30,10 +30,13 @@ def do_deploy(archive_path):
         name = file_name.split('.')[0]
         release_path = "/data/web_static/releases/" + name
         run("mkdir -p " + release_path)
-        run("tar -xzf /tmp/" + file_name + " -C " + release_path)
+        run("tar -xzf /tmp/" + file_name + " -C " + release_path + "/")
         run("rm /tmp/" + file_name)
+        run("mv " + release_path + "/web_static/* " + release_path + "/")
+        run("rm -rf " + release_path + "/web_static")
         run("rm -rf /data/web_static/current")
-        run("ln -s " + release_path + " /data/web_static/current")
+        run("ln -s " + release_path + "/ /data/web_static/current")
+        print("New version deployed!")
         return True
     except Exception:
         return False
