d5645460fb0d941abc320b82e9298eb4d480fb5f
TheKingsident <kingsident@gmail.com>
2024-01-27 16:07:43 +0100

    Added new files

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Starts a simple Flask web application"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """Displays content"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def disp_hbnb():
+    """Displays content"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def disp_text(text):
+    """Displays dynamic content"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def disp_python(text):
+    """Displays dynamic content"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def disp_number(n):
+    """Displays dynamic content"""
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def disp_numtemp(n):
+    """Displays dynamic content"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def disp_odd_even(n):
+    """Displays dynamic content"""
+    return render_template('6-number_odd_or_even.html', number=n, is_even=(n % 2 == 0))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
