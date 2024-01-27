2b3d94e7c3f8bbdf3d595d4dc4f47b69820e32e8
TheKingsident <kingsident@gmail.com>
2024-01-27 15:18:37 +0100

    Added new file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,5 +22,12 @@ def disp_text(text):
     return 'C ' + text.replace('_', ' ')
 
 
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def disp_python(text):
+    """Displays content"""
+    return 'Python ' + text.replace('_', ' ')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
