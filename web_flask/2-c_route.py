9b73287b09908c237c3bce8feede4222cc5bf22f
TheKingsident <kingsident@gmail.com>
2024-01-27 14:54:13 +0100

    Added new files

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a simple Flask web application"""
+from flask import Flask
+from sys import argv
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """Displays content"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def disp_hbnb():
+    """Displays content"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def disp_text(text):
+    """Displays content"""
+    return 'C '+ text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
