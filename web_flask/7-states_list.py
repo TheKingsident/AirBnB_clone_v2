653fe4238613270e6da92709ea42c0da84a31f3a
TheKingsident <kingsident@gmail.com>
2024-01-28 15:34:52 +0100

    Updated

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""starts a Flask web application that dispalys a list of all states in storage"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardownDB(exception):
+    """Tears down the database and remove SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays list of all states in the storage"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state:state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
